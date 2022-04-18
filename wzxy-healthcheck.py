bda2e32320a70e657c65d4f615d25e4e61207d76
2339808716 <103916204+2339808716@users.noreply.github.com>
2022-04-18 15:00:39 +0800

    Update wzxy-healthcheck.py

--- wzxy-healthcheck.py
+++ wzxy-healthcheck.py
@@ -6,7 +6,11 @@ import utils
 from urllib.parse import urlencode
 import time
 import hashlib
-
+import time
+import hashlib
+sign_time = int(round(time.time() * 1000)) #13位
+content = f"甘肃省_{t}_天水市"
+signature = hashlib.sha256(content.encode('utf-8')).hexdigest()
 
 class WoZaiXiaoYuanPuncher:
     def __init__(self):
@@ -91,6 +95,9 @@ class WoZaiXiaoYuanPuncher:
             "province": os.environ['WZXY_PROVINCE'],
             "township": os.environ['WZXY_TOWNSHIP'],
             "street": os.environ['WZXY_STREET'],
+            "city_code": "156620500",  # 20220417新增，广州
+            "timestampHeader": sign_time,  # 20220417新增，时间戳（13位）
+            "signatureHeader": signature  # 20220417新增，SHA256
         }
         data = urlencode(sign_data)
         self.session = requests.session()
